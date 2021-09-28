5f078500c0297400b02a09cba4f2bd65d7a54341
Marius Damian <86361141+MariusDamian@users.noreply.github.com>
2021-09-28 14:28:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let lat;
+let proc = "%";
+let btns = document.getElementById("btns");
+let btnd = document.getElementById("btnd");
+
+let votStanga = 50;
+btns.innerHTML = votStanga;
+
+function stanga() {
+     votStanga++;
+     btns.innerHTML = votStanga;
+     lat = (votStanga / (votDreapta + votStanga)) * 100;
+     document.getElementById("wdth").style.width = lat + proc;
+}
+
+let votDreapta = 50;
+btnd.innerHTML = votDreapta;
+
+function dreapta() {
+     votDreapta++;
+     btnd.innerHTML = votDreapta;
+     lat = (votStanga / (votDreapta + votStanga)) * 100;
+     document.getElementById("wdth").style.width = lat + proc;
+}
+
+console.log(btnd.innerHTML);
