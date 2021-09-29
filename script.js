6a214b8e8c07b46a5378a61c46cfd1b87ca465a0
Marius Damian <86361141+MariusDamian@users.noreply.github.com>
2021-09-29 14:20:13 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let proc = "%";
 let btns = document.getElementById("btns");
 let btnd = document.getElementById("btnd");
 let msg = document.getElementById("msg");
+let cont1 = document.getElementById("cont1");
+let cont2 = document.getElementById("cont2");
+let forms = document.getElementById("forms");
+let formd = document.getElementById("formd");
 
 let votStanga = 30;
 btns.innerHTML = votStanga;
@@ -13,7 +17,7 @@ function stanga() {
      lat = (votStanga / (votDreapta + votStanga)) * 100;
      document.getElementById("wdth").style.width = lat + proc;
      if (lat > 58) {
-          msg.innerHTML = "It looks like the red team might win!";
+          msg.innerHTML = `"It looks like the ${cont1.innerHTML} team might win!"`;
      }
 }
 
@@ -26,6 +30,41 @@ function dreapta() {
      lat = (votStanga / (votDreapta + votStanga)) * 100;
      document.getElementById("wdth").style.width = lat + proc;
      if (lat < 42) {
-          msg.innerHTML = "It looks like the blue team might win!";
+          msg.innerHTML = `"It looks like the ${cont2.innerHTML} team might win!"`;
      }
 }
+
+function addCont() {
+     if (forms.value === "" || formd.value === "") {
+          alert("Please add contestants!");
+          return;
+     }
+     cont1.innerHTML = forms.value;
+     cont2.innerHTML = formd.value;
+     document.getElementById("forms").value = "";
+     document.getElementById("formd").value = "";
+     votDreapta = 30;
+     btnd.innerHTML = "30";
+     btns.innerHTML = "30";
+     votStanga = 30;
+     document.getElementById("wdth").style.width = "50%";
+}
+
+// let arr = ["manusaa", "caciula", "fular"];
+
+// let arrText = document.getElementById("arrText");
+
+// arrText.innerHTML = arr;
+
+// function pp() {
+//                if (document.getElementById("arrInput").value === "") {
+//                     alert("qwe");
+//                     return
+//                }
+//      let arrInput = document.getElementById("arrInput").value;
+//      arr.push(arrInput);
+//      arrText.innerHTML = arr;
+//      console.log(arr);
+//      document.getElementById("arrInput").value = "";
+
+// }
